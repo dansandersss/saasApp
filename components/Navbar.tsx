316dbb6f7a816c602145ea75295bbed0f871801b
Dan Sanders <im.dan.01@mail.ru>
2025-07-08 09:52:05 +0300

    implemented auth, billing, database setup, create companion server

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,14 @@
-import { Link } from "lucide-react";
+import { Link, User } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 import NavItems from "./NavItems";
+import {
+  SignIn,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
@@ -13,7 +20,15 @@ const Navbar = () => {
       </Link>
       <div className="flex items-center gap-8">
         <NavItems />
-        <p>Sign In</p>
+        <SignedOut>
+          <SignInButton>
+            <button className="btn-signin">Sign In</button>
+          </SignInButton>
+        </SignedOut>
+
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
     </nav>
   );
